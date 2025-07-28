a15d104d6d9b48a7e136119c52d4dcad5af59d8c
dak501 <dakirsch@yahoo.com>
2025-07-28 08:23:16 -0400

    current server

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def sent_emotion_detector():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+    #    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    return response
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
